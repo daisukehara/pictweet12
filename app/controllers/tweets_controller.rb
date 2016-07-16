7f76dad7e29ad3eb4e84d8c022bdab39a4a5a624
daisukehara <hara.dsk.usa@gmail.com>
2016-07-17 08:44:39 +0900

    devise関連をまとめて

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: :index
 
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
@@ -32,6 +33,10 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:image, :text)
+  end
+
+  def move_to_index
+    redirect_to controller: :tweets, action: :index unless user_signed_in?
   end
 
   
