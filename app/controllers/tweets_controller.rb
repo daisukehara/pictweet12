a8711a104da418fecf0ef71c742ecb0b02ea3dfa
daisukehara <hara.dsk.usa@gmail.com>
2016-07-16 13:50:00 +0900

    tweet controllerの記述を書く。ただし、deviseはまだ作っていないので、current_userに関するものは記載していない

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,24 +1,37 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
+    Tweet.creat(tweet_params)
   end
 
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
   end
 
   def edit
+    tweet = Tweet.find(params[:id])
   end
 
   def update
+    tweet = Tweet.find(params[:id])
+    tweet.upda
   end
 
   def show
+    tweet = Tweet.find(params[:id])
+  end
+
+  private
+  def tweet_params
+    params.permit(:image, :text)
   end
 
   
