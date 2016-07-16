b8a7302f535d6bee91aada6040346dc6a2a20309
daisukehara <hara.dsk.usa@gmail.com>
2016-07-16 14:16:42 +0900

    index, new, createを実装（テーブルに誤りがあったため、そこも修正）

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716043142) do
+ActiveRecord::Schema.define(version: 20160716051351) do
 
-  create_table "controllers", force: :cascade do |t|
-    t.string   "tweet",      limit: 255
+  create_table "tweets", force: :cascade do |t|
     t.text     "image",      limit: 65535
     t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
