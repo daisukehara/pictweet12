dfbb239a0aa53193ef80722099f46d87dc693ce1
daisukehara <hara.dsk.usa@gmail.com>
2016-07-17 11:59:26 +0900

    コメント関連実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   resources :users, only: [:show]
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
 end
